c97c97b502eabd21fbf961494ccb83b9c3d07ac2
Huangstt <a2902511@gmail.com>
2023-08-15 17:18:30 +0800

    刪掉不需要的檔案，加入商品管理後台

--- mall/product/backstage/js/sublist.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $(li.sidebar-headar).on("click", function(e){
-        e.preventDefault();
-        $(this).closest("i").toggleClass("-on");
-        $(this).closest("i").find("div.inner_block").slideToggle();
-    })
-
-})();
