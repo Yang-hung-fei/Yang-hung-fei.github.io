c7373a949ee29ebdbdf3d5eef0583784d6dd5531
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-08-22 09:49:29 +0800

    notify

--- frontend/js/userPic.js
+++ frontend/js/userPic.js
@@ -1,30 +1,28 @@
 import config from "../../ipconfig.js";
-      $(window).on("load", () => {
-        var token = localStorage.getItem("Authorization_U");
+$(window).on("load", () => {
+  var token = localStorage.getItem("Authorization_U");
 
-        fetch(config.url + "/user/profile", {
-          method: "GET",
-          headers: {
-            Authorization_U: token,
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => response.json())
-          .then((responseData) => {
-            var code = responseData.code;
-            if (code === 200) {
-              let userPic_base64 = responseData.message.userPic;
-              console.log(responseData);
-              console.log(userPic_base64);
-              let user_el = document.getElementById("user");
-              user_el.style.background =
-                "url('data:image/png;base64," +
-                userPic_base64 +
-                "') !important";
-            }
-          })
-          .catch((error) => {
-            // 处理捕获的错误，包括网络错误等
-            console.error("Fetch error:", error);
-          });
-      });
+  fetch(config.url + "/user/profile", {
+    method: "GET",
+    headers: {
+      Authorization_U: token,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((responseData) => {
+      var code = responseData.code;
+      if (code === 200) {
+        let userPic_base64 = responseData.message.userPic;
+        console.log(responseData);
+        console.log(userPic_base64);
+        let user_el = document.getElementById("user");
+        user_el.style.background =
+          "url('data:image/png;base64," + userPic_base64 + "') !important";
+      }
+    })
+    .catch((error) => {
+      // 处理捕获的错误，包括网络错误等
+      console.error("Fetch error:", error);
+    });
+});
