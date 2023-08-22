cd1d3f322d7df69a5ff0b1457e13b1e255a89aa5
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-08-22 09:23:10 +0800

    notify

---
+++ frontend/js/notify.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("notifyButton 被點擊");
+    const notificationDiv = document.querySelector("#notifyMenu.dropdown-menu");
+    const notifyButton = document.getElementById("notify");
+  
+    notifyButton.addEventListener("click", function () {
+      notificationDiv.classList.toggle("show");
+    });
+  
+    // 點擊其他區域時隱藏
+    document.addEventListener("click", function (event) {
+      if (
+        !notificationDiv.contains(event.target) &&
+        !notifyButton.contains(event.target)
+      ) {
+        notificationDiv.classList.remove("show");
+      }
+    });
+  });
+  
