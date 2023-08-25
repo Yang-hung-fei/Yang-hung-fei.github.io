69a9d9e6621d5c4c0f66d4593d052448ca7bf32c
Yang-hung-fei <x52131221@gmail.com>
2023-08-25 17:07:28 +0800

    appoC ver.2

--- frontend/pages/memberCentre/js/appointmentRecord.js
+++ frontend/pages/memberCentre/js/appointmentRecord.js
@@ -77,20 +77,76 @@ window.addEventListener("load", () => {
                 word-wrap: break-word;">${appointment.pgaNotes}</td>
                 <td class="text-center">${appointment.userName}</td>
                 <td class="text-center" name="pgaPhone">${appointment.pgaPhone}</td>
-                <td class="text-center" name="pgaState">${appointment.pgaState}</td>
+                <td class="text-center state" name="pgaState" style="font-size:15px;">${appointment.pgaState}</td>
                 <td class="text-center disable-edit">
-                    <button class="btn slot-button" data-action="modify">修改</button>
+                    <button class="btn slot-button modify">修改</button>
                 </td>
                 <td class="text-center">
-                    <button class="btn slot-button">完成</button>
+                    <button class="btn slot-button finish">完成</button>
                 </td>
                 <td class="text-center">
-                    <button class="btn slot-button">取消</button>
+                    <button class="btn slot-button cancel">取消</button>
                 </td>
             `;
-
             tableBody.appendChild(row);
+            if (appointment.pgaState === "已完成") {
+                const stateElement =row.querySelectorAll(".state");
+                stateElement.forEach(stateElement => {
+                    stateElement.style.color = 'green'
+                });
+            }
+            if (appointment.pgaState === "已取消") {
+                const stateElement2 =row.querySelectorAll(".state");
+                stateElement2.forEach(stateElement2 => {
+                    stateElement2.style.color = 'red'
+                });
+            }
+
+            if (appointment.pgaState === "已取消" || appointment.pgaState === "已完成") {
+                const finishButton = row.querySelectorAll(".finish");
+                const cancelButton = row.querySelectorAll(".cancel");
+                const editButton = row.querySelectorAll(".modify");
+                
+                finishButton.forEach(finishButton => {
+                    finishButton.disabled = true;
+                });
+                
+                cancelButton.forEach(cancelButton => {
+                    cancelButton.disabled = true;
+                });
+                editButton.forEach(editButton => {
+                    editButton.disabled = true;
+                });
+            }
+
+            const editButtons = tableBody.querySelectorAll("tr > td > .modify");
+
+            editButtons.forEach((editButton) => {
+
+                editButton.addEventListener("click", () => {
+                    handleModifyButtonClick(editButton);
+                });
+            });
+            const finishButtons = tableBody.querySelectorAll("tr > td > .finish");
+
+            finishButtons.forEach((finishButtons) => {
+
+                finishButtons.addEventListener("click", () => {
+                    
+                });
+            });
+            const cancelButtons = tableBody.querySelectorAll("tr > td > .cancel");
+
+            cancelButtons.forEach((cancelButtons) => {
+
+                cancelButtons.addEventListener("click", () => {
+                    
+                });
+            });
+
+
         });
+        
     }
 
     function updatePaginationButtons(totalAppointments) {
@@ -138,44 +194,185 @@ window.addEventListener("load", () => {
         requestAnimationFrame(scroll);
     }
 
+    //查詢班表(token要改)
+    function fetchScheduleForDate(pgId, dateInput, timeSlotsContainer) {
+        fetch(config.url + `/user/pgScheduleForA?pgId=${pgId}`, {
+            method: "GET",
+            headers: {
+                Authorization_U: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiZXhwIjoxNjkzMTMwNjQyfQ.65VWBEyaA6_Wq8LB8zkO1xT1TxlRsbyJHI-uwNKhWqs",
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.code === 200) {
+                    const scheduleDates = data.message.map(schedule => schedule.pgsDate);
+                    const minDate = scheduleDates[0];
+                    const maxDate = scheduleDates[scheduleDates.length - 1];
+
+                    dateInput.flatpickr({
+                        minDate: minDate,
+                        maxDate: maxDate,
+                        dateFormat: "Y-m-d",
+                        onChange: function (selectedDates, dateStr, instance) {
+                            const selectedDate = selectedDates[0];
+                            const formattedDate = formatDate(selectedDate);
+                            dateInput.value = formattedDate;
+
+                            // 找到選中日期的班表資料
+                            const selectedSchedule = data.message.find(schedule => schedule.pgsDate === formattedDate);
+                            if (selectedSchedule) {
+                                //pgsIdLabel.style.display = "block";
+                                generateTimeSlots(selectedSchedule.pgsState, timeSlotsContainer);
+                            } else {
+                                //pgsIdLabel.style.display = "none";
+                            }
+                        },
+                    });
+                }
+            });
+    }
+    //抓對應時間
+    function generateTimeSlots(pgsState, timeSlotsContainer) {
+        timeSlotsContainer.innerHTML = '';
+
+        for (let i = 0; i < 24; i++) {
+            // 创建<option>元素
+            const option = document.createElement('option');
+            option.value = `${i}:00 ~ ${i + 1}:00`;
+            option.textContent = `${i}:00 ~ ${i + 1}:00`;
+
+            // 设置不可选择的选项
+            if (pgsState[i] === '1' || pgsState[i] === '2') {
+                option.disabled = true;
+            }
+
+            // 添加到<select>元素中
+            timeSlotsContainer.appendChild(option);
+        }
+    }
+    //格式化yyyy-mm-dd
+    function formatDate(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        return `${year}-${month}-${day}`;
+    }
 
     //修改:
     function handleModifyButtonClick(editButton) {
 
-        
         const row = editButton.closest("tr");
+
+        const pgId = row.querySelector("[name=pgId]").textContent;
+        const pgaNo = row.querySelector("[name=pgaNo]").textContent;
+
         const sourcePgaDate = row.querySelector("[name=sourcePgaDate]").textContent;
         const sourcePgaTime = row.querySelector("[name=sourcePgaTime]").textContent;
         const pgaOption = row.querySelector("[name=pgaOption]").textContent;
         const pgaNotes = row.querySelector("[name=pgaNotes]").textContent;
         const pgaPhone = row.querySelector("[name=pgaPhone]").textContent;
-
         Swal.fire({
             title: '修改預約',
             html:
-                `<input type="text" id="editPgaNewDate" placeholder="新預約日期" value="${sourcePgaDate}">
-                 <input type="text" id="editPgaNewTime" placeholder="新预约时段" value="${sourcePgaTime}">
-                 <input type="text" id="editPgaOption" placeholder="新预约选项" value="${pgaOption}">
-                 <input type="text" id="editPgaNotes" placeholder="新备注" value="${pgaNotes}">
-                 <input type="text" id="editPgaPhone" placeholder="新预约电话" value="${pgaPhone}">`,
+                `
+                <div class="row d-flex justify-content-center" style="color: #fff; font-family: cwTeXYen" >
+                    <div class="col-md-6" style="margin-top:25px;">
+                        <label for="sourcePgaDate" style="font-size: 18px;">原預約日期</label>
+                        <input type="text" id="sourcePgaDate"  value="${sourcePgaDate}" readonly><br>
+                        <label for="sourcePgaTime" style="font-size: 18px;">原預約時間</label>               
+                        <input type="text" id="sourcePgaTime"  value="${sourcePgaTime}" readonly><br>
+                        <label for="editPgaOption" style="font-size: 18px;">原預約選項</label>  
+                        <input type="text" id="editPgaOption"  value="${pgaOption}" readonly><br>
+                        <label for="editPgaNotes" style="font-size: 18px;">原預約備註</label>
+                        <input type="text" id="editPgaNotes"  value="${pgaNotes}" readonly style="max-height:100px; min-height:100px;"><br>
+                        <label for="editPgaPhone" style="font-size: 18px;">原手機號碼</label>
+                        <input type="text" id="editPgaPhone"  value="${pgaPhone}" readonly>
+                    </div>
+                    <div class="col-md-6">
+                    <span  style="font-size: 14px; color:#ddd; font-family: cwTeXYen" >不修改的內容請留空</span><br>
+                        <input id="pgId" name="pgId" value="${pgId}" hidden readonly></input>
+                        <input id="pgaNo" name="pgaNo" value="${pgaNo}" hidden readonly></input>
+                        <label for="dateInput" style="font-size: 18px;">修改日期</label>
+                        <input type="text" id="dateInput" name="dateInput" placeholder="請點擊選擇預約日期"><br>
+                        
+                        <label for="timeSlotsContainer" style="font-size: 18px;">修改時間</label><br>              
+                        <select id="timeSlotsContainer" name="pgaNewTime"></select><br>
+                        
+                        <label for="pgaOption" style="font-size: 18px;">修改選項</label>
+                        <select id="pgaOption">
+                            <option value="" disabled selected>如需要請選擇</option>
+                            <option value="狗狗洗澡" >狗狗洗澡</option>
+                            <option value="狗狗半手剪 (洗澡+剃毛)" >狗狗半手剪 (洗澡+剃毛)</option>
+                            <option value="狗狗全手剪(洗澡+全身手剪造型)" >狗狗全手剪(洗澡+全身手剪造型)</option>
+                            <option value="貓咪洗澡" >貓咪洗澡</option>
+                            <option value="貓咪大美容" >貓咪大美容</option>
+                        </select><br>
+                        <label for="pgaNotes" style="font-size: 18px;">修改備註</label>
+                        <textarea name="pgaNotes" id="pgaNotes" rows="10"
+                        style="min-width: 200; max-width: 200px; max-height:100px; resize: none; overflow: auto;"
+                        maxlength="500"></textarea><br>
+                        <label for="pgaPhone" style="font-size: 18px;">修改手機號碼</label>
+                        <input type="text" id="pgaPhone">
+                    </div>
+                 </div>
+                 `,
+            didRender: function () {
+                const timeSlotsContainer = document.getElementById('timeSlotsContainer');
+                const dateInput = document.getElementById('dateInput');
+                fetchScheduleForDate(pgId, dateInput, timeSlotsContainer);
+            },
             showCancelButton: true,
             confirmButtonText: '確認',
+            confirmButtonColor: '#e4b074',
             cancelButtonText: '取消',
+            background: '#997b66',
             preConfirm: () => {
                 const editedData = {
-                    pgaNewDate: document.getElementById("editPgaNewDate").value,
-                    pgaNewTime: document.getElementById("editPgaNewTime").value,
-                    pgaOption: document.getElementById("editPgaOption").value,
-                    pgaNotes: document.getElementById("editPgaNotes").value,
-                    pgaPhone: document.getElementById("editPgaPhone").value,
+                    pgaNo: document.getElementById("pgaNo").value,//notnull
+                    pgId: document.getElementById("pgId").value,//notnull
+                    sourcePgaDate: document.getElementById("sourcePgaDate").value,
+                    pgaNewDate: document.getElementById("dateInput").value,
+                    sourcePgaTime: document.getElementById("sourcePgaTime").value,
+                    pgaNewTime: document.getElementById("timeSlotsContainer").value,
+                    pgaOption: document.getElementById("pgaOption").value,
+                    pgaNotes: document.getElementById("pgaNotes").value,
+                    pgaPhone: document.getElementById("pgaPhone").value,
                 };
                 return editedData;
             },
         }).then((result) => {
             if (result.isConfirmed) {
-                // 在这里可以将修改后的数据发送到服务器
-                // 更新表格等逻辑
+                // 送出修改，TOKEN要改
+                const editedData = result.value;
+        
+                fetch(config.url + "/user/modifyAppointment", {
+                    method: "POST",
+                    headers: {
+                        Authorization_U: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiZXhwIjoxNjkzMTMwNjQyfQ.65VWBEyaA6_Wq8LB8zkO1xT1TxlRsbyJHI-uwNKhWqs",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(editedData)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.code === 200) {
+                        Swal.fire({
+                            icon: "success",
+                            title: "預約修改成功",
+                            text: data.message
+                        });
+                        fetchAndBuildTable(itemsPerPage, sortSelect.value, currentPage);
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "預約修改失敗",
+                            text: data.message
+                        });
+                    }
+                });
             }
+
         });
     }
 
