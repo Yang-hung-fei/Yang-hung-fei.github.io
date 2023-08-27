f9b0fcbdd37790a3f00ce620f9d7ecd0bee4824f
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-08-28 07:13:35 +0800

    login swel & logout button

---
+++ frontend/js/headerLogoutBtn.js
@@ -0,0 +1,39 @@
+// Get the user and logout button elements
+const userIcon = document.getElementById("userIcon");
+const logoutButton = document.getElementById("logoutButton");
+
+logoutButton.addEventListener("click", ()=>{
+  localStorage.removeItem("Authorization_U");
+})
+
+let timeoutId; // To store the timeout ID
+
+// Show the logout button when user icon is hovered
+userIcon.addEventListener("mouseover", () => {
+  clearTimeout(timeoutId); // Clear any existing timeouts
+  logoutButton.style.opacity = "1";
+  logoutButton.style.pointerEvents = "auto";
+});
+
+// Hide the logout button with a delay when user icon is not hovered
+userIcon.addEventListener("mouseout", () => {
+  clearTimeout(timeoutId); // Clear any existing timeouts
+  timeoutId = setTimeout(() => {
+    logoutButton.style.opacity = "0";
+    logoutButton.style.pointerEvents = "none";
+  }, 500); // Adjust the delay time (in milliseconds) as needed
+});
+
+// Hide the logout button when the button itself is hovered
+logoutButton.addEventListener("mouseover", () => {
+  clearTimeout(timeoutId); // Clear any existing timeouts
+});
+
+// Hide the logout button when the button itself is not hovered
+logoutButton.addEventListener("mouseout", () => {
+  clearTimeout(timeoutId); // Clear any existing timeouts
+  timeoutId = setTimeout(() => {
+    logoutButton.style.opacity = "0";
+    logoutButton.style.pointerEvents = "none";
+  }, 500); // Adjust the delay time (in milliseconds) as needed
+});
--- frontend/pages/user/js/login.js
+++ frontend/pages/user/js/login.js
@@ -7,7 +7,7 @@ $(window).on("load", () => {
   if (token) {
     window.location.href = "/frontend/pages/memberCentre/memberCentre.html";
   } else {
-    $("#localLogin").on("click", (event) => {
+    $("#localLogin").on("click", async (event) => {
       event.preventDefault(); // 阻止表單的預設行為
       var emailText = $("#email").val();
       var password = $("#password").val();
@@ -20,17 +20,23 @@ $(window).on("load", () => {
       var emailError = checkEmail(emailText);
       var passwordError = checkPassword(password);
 
-      if (emailError) {
-        alert(emailError);
-        return;
+      if (emailError || passwordError) {
+        swalThenFadeOut(emailError || passwordError);
+        return; // 如果有錯誤，中斷程式碼執行
       }
 
-      if (passwordError) {
-        alert(passwordError);
-        return;
-      }
+      // 顯示 fetch 前的燈箱
+      $("div.overlay").fadeIn();
+      
+      try {
+        await localLogin(emailText, password);
 
-      localLogin(emailText, password);
+        // 在 localLogin 完成後，關閉燈箱
+        $("div.overlay").fadeOut();
+      } catch (error) {
+        console.error("Error:", error);
+        $("div.overlay").fadeOut();
+      }
     });
   }
 });
@@ -41,65 +47,67 @@ export function checkEmail(emailText) {
     /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
 
   if (!emailText.match(emailRegex)) {
-    swal("請輸入信箱格式。");
+    return "Email 格式不正確。";
   }
   return null; // No error
 }
 
 function checkPassword(password) {
   if (password.length < 6) {
-    swal("請確認密碼，須至少6個英文或數字。");
+    return "密碼長度不足。";
   }
   return null; // No error
 }
 
-function localLogin(email, password) {
-  $("div.overlay").fadeIn();
-
+async function localLogin(email, password) {
   const data = {
     email: email,
     password: password,
   };
 
-  fetch(config.url + "/user/login", {
+  const response = await fetch(config.url + "/user/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((responseData) => {
-      var code = responseData.code;
+  });
 
-      if (code === 200) {
-        var token = responseData.message;
-        localStorage.setItem("Authorization_U", token);
-        if (token) {
-          backToRedirectUrl();
-        } else {
-          swal({
-            text: "登入錯誤，請聯繫客服信箱。",
-            onAfterClose: () => {
-              console.log("afterclose");
-              $("div.overlay").fadeOut(); // 在swal关闭后关闭燈箱
-              setTimeout(function () {
-                window.location.href = "/frontend/index.html";
-              }, 1000);
-            },
-          });
-        }
-      } else if (code === 400) {
-        console.log("Code 400 response:", data.message);
-        swal("密碼錯誤");
-        $("div.overlay").fadeOut();
-      } else {
-        console.log("Unknown response code:", data.code);
-      }
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-      swal("網路錯誤，請稍後再試。");
+  const responseData = await response.json();
+
+  var code = responseData.code;
+
+  if (code === 200) {
+    var token = responseData.message;
+    localStorage.setItem("Authorization_U", token);
+    if (token) {
+      backToRedirectUrl();
+    } else {
+      await swalThenFadeOut("登入錯誤，請聯繫客服。");
+    }
+  } else if (code === 400) {
+    console.log("Code 400 response:", data.message);
+    await swalThenFadeOut("帳號未註冊，或密碼錯誤。");
+  } else {
+    console.log("Unknown response code:", data.code);
+  }
+}
+
+async function swalThenFadeOut(message) {
+  await swal({
+    text: message,
+    onAfterClose: () => {
+      console.log("afterclose");
       $("div.overlay").fadeOut();
-    });
+    },
+    buttons: {
+      confirm: {
+        text: "確認",
+        value: true,
+        visible: true,
+        className: "",
+        closeModal: true,
+      },
+    },
+  });
 }
