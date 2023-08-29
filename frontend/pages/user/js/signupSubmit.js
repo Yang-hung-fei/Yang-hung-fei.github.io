27147466941acb15876160a59c5d68e36b8ff0bb
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-08-29 14:08:17 +0800

    redirectUrl

--- frontend/pages/user/js/signupSubmit.js
+++ frontend/pages/user/js/signupSubmit.js
@@ -19,9 +19,8 @@ $("#submit").on("click", () => {
       if (code === 200) {
         console.log("code", code, ":", responseData.message);
 
-        
-        swal("註冊成功", `將在 ${delayTime / 1000} 秒後跳至首頁。`, "success"); // 顯示註冊成功的提示框
         const delayTime = 5000; // 5秒 // 設定延遲時間（毫秒）
+        swal("註冊成功", `將在 ${delayTime / 1000} 秒後跳至登入頁面。`, "success"); // 顯示註冊成功的提示框
         const countdownElement = document.querySelector(".swal-text"); // 取得文字內的元素
         let countdown = Math.ceil(delayTime / 1000); // 開始倒數
         const countdownInterval = setInterval(() => {
@@ -31,7 +30,8 @@ $("#submit").on("click", () => {
           if (countdown <= 0) {
             clearInterval(countdownInterval);
             // 跳轉至首頁
-            window.location.href = "/frontend/pages/memberCentre/memberCentre.html";
+            window.location.href =
+              "/frontend/pages/user/login.html";
           }
         }, 1000); // 每秒更新一次倒數
 
@@ -43,9 +43,11 @@ $("#submit").on("click", () => {
     .catch((error) => {
       console.error("Error:", error);
       // 输出服务器返回的文本
-      error.text().then((text) => {
-        console.error("Server response:", text);
-      });
+      if (error instanceof Response) {
+        error.text().then((text) => {
+          console.error("Server response:", text);
+        });
+      }
     });
 });
 
@@ -55,24 +57,24 @@ function getSignUpData() {
   var gender_el = document
     .querySelector("#gender + span span span span")
     .getAttribute("title");
-  var birthday_el = document.getElementById("birthday").value; // 获取输入框中的日期字符串，例如 "21/08/2023"
+  // var birthday_el = document.getElementById("birthday").value; // 获取输入框中的日期字符串，例如 "21/08/2023"
 
-  // 将日期字符串分割成日、月和年
-  var parts = birthday_el.split("/");
-  var day = parseInt(parts[0]);
-  var month = parseInt(parts[1]);
-  var year = parseInt(parts[2]);
+  // // 将日期字符串分割成日、月和年
+  // var parts = birthday_el.split("/");
+  // var day = parseInt(parts[0]);
+  // var month = parseInt(parts[1]);
+  // var year = parseInt(parts[2]);
 
-  // 创建新的日期对象（注意月份减去 1）
-  var newDate = new Date(year, month - 1, day);
+  // // 创建新的日期对象（注意月份减去 1）
+  // var newDate = new Date(year, month - 1, day);
 
-  // 获取年、月、日的字符串形式
-  var yyyy = newDate.getFullYear();
-  var mm = (newDate.getMonth() + 1).toString().padStart(2, "0"); // 月份需要加1，然后保证两位数格式
-  var dd = newDate.getDate().toString().padStart(2, "0"); // 日期保证两位数格式
+  // // 获取年、月、日的字符串形式
+  // var yyyy = newDate.getFullYear();
+  // var mm = (newDate.getMonth() + 1).toString().padStart(2, "0"); // 月份需要加1，然后保证两位数格式
+  // var dd = newDate.getDate().toString().padStart(2, "0"); // 日期保证两位数格式
 
-  // 组合成目标格式的字符串
-  var formattedDate = yyyy + "-" + mm + "-" + dd;
+  // // 组合成目标格式的字符串
+  // var formattedDate = yyyy + "-" + mm + "-" + dd;
 
   var email_el = document.getElementById("email").value;
   var phone_el = document.getElementById("phone").value;
@@ -97,7 +99,7 @@ function getSignUpData() {
 
   var userData = {};
 
-  userData.chptcha = chaptcha_el;
+  userData.captcha = chaptcha_el;
   userData.userAddress = fullAddress;
   userData.userEmail = email_el;
   if (gender_el === "女性") {
