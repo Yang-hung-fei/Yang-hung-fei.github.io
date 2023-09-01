45e74517b08e464af5e858738be4833077d4346b
Yang-hung-fei <x52131221@gmail.com>
2023-09-01 14:51:41 +0800

    sc and le var2

--- backend/pages/pgPersonalManagement/leaveSendAndCheck/js/leaveSendAndCheck.js
+++ backend/pages/pgPersonalManagement/leaveSendAndCheck/js/leaveSendAndCheck.js
@@ -9,7 +9,7 @@ window.addEventListener("load", () => {
 
     // 撈所有預約單 for Manager
     function fetchAndBuildTable() {
-        fetch(config.url + `/manager/findAllLeave`, {
+        fetch(config.url + `/manager/getLeaveByPg`, {
             method: "GET",
             headers: {
                 Authorization_M: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiZXhwIjoxNjkzNzM0ODgzfQ.MGVymnvxKaRZ9N7gGInQitt7q_zVoHxvt2n7hoPws6A", // 使用Manager Token
@@ -72,19 +72,7 @@ window.addEventListener("load", () => {
                         return rows.join('<br>'); // Display rows with line breaks
                     }
                 },
-                { data: 'leaveState' },
-                {
-                    data: null,
-                    render: function (data, type, row) {
-                        return `<button class="btn slot-button finish" data-leaveNo="${row.leaveNo}" value="1">通過</button>`;
-                    }
-                },
-                {
-                    data: null,
-                    render: function (data, type, row) {
-                        return `<button class="btn slot-button cancel" data-leaveNo="${row.leaveNo}" value="2">取消</button>`;
-                    }
-                }
+                { data: 'leaveState' }
             ],
             order: [[0, 'asc']], // 預設按 leaveCreated 欄位降序排列
             paging: true,
@@ -105,48 +93,8 @@ window.addEventListener("load", () => {
             }
         });
         $('.table-container').css('overflow-x', 'auto');
-        $('.table-fill tbody').on('click', '.finish', function (event) {
-
-
-            const leaveNo = $(event.target).attr('data-leaveNo');
-            const action = $(event.target).val();
-            Swal.fire({
-                title: '確定審核通過嗎?',
-                text: "你將無法在次變動此假單!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: '確定!',
-                cancelButtonText:'取消'
-              }).then((result) => {
-                if (result.isConfirmed) {
-                    changeLeave(leaveNo, action);
-                }
-              })
-
-            
-            // 執行批准假單的函數
-        });
-
-        $('.table-fill tbody').on('click', '.cancel', function (event) {
-            const leaveNo = $(event.target).attr('data-leaveNo');
-            const action = $(event.target).val();
-            Swal.fire({
-                title: '確定取消此假單嗎?',
-                text: "你將無法在次變動此假單!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: '確定!',
-                cancelButtonText:'取消'
-              }).then((result) => {
-                if (result.isConfirmed) {
-                    changeLeave(leaveNo, action);
-                }
-              })
-        });
+        
+        
     }
 
 
@@ -184,7 +132,6 @@ window.addEventListener("load", () => {
                         title: "假單審核失敗",
                         text: data.message
                     });
-                    fetchAndBuildTable();
                 }
             });
     }
