3a7c5898ed3c5300e522ed6846963ee9f22dbd0f
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-09-01 16:41:52 +0800

    fixing addManager

--- backend/pages/manageManager/js/addManager.js
+++ backend/pages/manageManager/js/addManager.js
@@ -25,8 +25,44 @@ export function addManager() {
 }
 
 function getNewManagerProfile() {
-    const newManagerAccount;
-    const newManagerPassword;
+  const newManagerAccount = "";
+  const newManagerPassword = "";
 
-    let newManagerProfile;
+  let newManagerProfile;
 }
+
+function creatNewManager() {
+  swal({
+    text: '新增管理員',
+    content: "input",
+    button: {
+      text: "Search!",
+      closeModal: false,
+    },
+  })
+    .then((name) => {
+      if (!name) throw null;
+
+      return fetch(`https://itunes.apple.com/search?term=${name}&entity=movie`);
+    })
+    .then((results) => {
+      return results.json();
+    })
+    .then((json) => {
+      const movie = json.results[0];
+
+      if (!movie) {
+        return swal("No movie was found!");
+      }
+      swal("新增成功", "", "success");
+    })
+    .catch((err) => {
+      if (err) {
+        swal("新增失敗", "", "error");
+      } else {
+        swal.stopLoading();
+        swal.close();
+      }
+    });
+}
+
