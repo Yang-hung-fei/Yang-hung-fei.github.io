03e5d256a5f803ad97b8d2f08e63cd91bdcb1804
CHEN YI TING <abc10512106@gmail.com>
2023-09-04 17:11:52 +0800

    activity fetch get complete

--- backend/pages/socialMedia/Activity/js/activityManager.js
+++ backend/pages/socialMedia/Activity/js/activityManager.js
@@ -1,4 +1,4 @@
-import config from '../../../../../ipconfig.js';
+import config from "../../../../../ipconfig.js";
 const hostUrl = config.url;
 const AcUrl = "/manager/activity";
 const activityId = "1";
@@ -37,18 +37,23 @@ function getAllAc() {
             return res.json();
         }).then(data => {
             let activityPageData = data.message;
-            //建立分頁
-            let totalPage = activityPageData.totalPage;
-            //一頁有幾筆資料
-            let pageSize = activityPageData.pageSize;
-            //目前所在頁數(預設為0)
-            let curentPage = activityPageData.currentPageNumber + 1;
-            let acitivityData = activityPageData.resList;
+            let pageData = {
+                //目前所在頁數(預設為0)
+                curentPage: activityPageData.currentPageNumber + 1,
+                //一頁有幾筆資料
+                pageSize: activityPageData.pageSize,
+                totalPage: activityPageData.totalPage,
+                fetchData: activityPageData.resList,
+            }
+            return pageData;
 
+        }).then(pageData => {
+            //create table data and pagination
+            createDataTable(pageData);
         })
         .catch(err => {
             console.error(err.message);
-        })
+        });
 }
 
 getAllAc();
@@ -57,61 +62,52 @@ getAllAc();
 
 
 
-// query string 
+
 /************************ 建立分頁************************/
-const pageData = {
-    curentPage: 1,
-    pageSize: 10,
-    totalPage: 2,
-    fetchData: []
-}
-
-
-// 選擇分頁元素
-const tbody = document.querySelector("tbody");
-const pagination = document.getElementById("pagination");
 
 // 更新資料表格和分頁
 function createDataTable(data) {
+    // // 選擇分頁元素
+    let table = document.querySelector("#table");
+    let tbody = table.querySelector("tbody");
+    let pagination = document.querySelector(".pagination");
+    console.log(pagination);
     // 清空tbody表格
     tbody.innerHTML = "";
+    let dataList = '';
     let fetchData = data.fetchData;
     // 建立資料表格
-    for (let i = 0; i < fetchData.length; i++) {
-        //拿到Fetch資料
-        let dataDetails = fetchData[i];
-        // 建立資料清單列
-        let row = document.createElement("tr");
+    fetchData.forEach(dataDetails => {
         // 建立資料
-        row.innerHTML = 
-        tbody.appendChild("");
+        dataList += `
+               <tr>
+               <td>${dataDetails.activityId}</td>
+               <td>${dataDetails.title}</td>
+               <td>${dataDetails.content}</td>
+               <td>${dataDetails.activityTime}</td>
+               <td>${dataDetails.enrollLimit}</td>
+               <td>${dataDetails.peopleCount}</td>
+               <td>${dataDetails.startTime}</td>
+               <td>${dataDetails.endTime}</td>
+               <td>${dataDetails.status == 0 ? "執行中" : "已取消"}</td>
+               <td><a href="#editActivity" class="edit" data-bs-toggle="modal"><i class="bi bi-pencil-square"></i>
+               </td>
+               <td><a href="#cancelActivity" class="cancel" data-bs-toggle="modal"><i
+                           class="bi bi-trash3-fill"></i>
+               </td>
+               <td><a href="#queryActivity" class="query" data-bs-toggle="modal"><i class="bi bi-eye-fill"></i>
+               </td>
+               </tr>`;
 
-        // <td>活動Id</td>
-        // <td>活動主題</td>
-        // <td>活動內容</td>
-        // <td>活動時間</td>
-        // <td>活動限制人數</td>
-        // <td>目前報名人數</td>
-        // <td>報名開始時間</td>
-        // <td>報名結束時間</td>
-        // <td>活動狀態</td>
-        // <td><a href="#editActivity" class="edit" data-bs-toggle="modal"><i class="bi bi-pencil-square"></i>
-        // </td>
-        // <td><a href="#cancelActivity" class="cancel" data-bs-toggle="modal"><i
-        //             class="bi bi-trash3-fill"></i>
-        // </td>
-        // <td><a href="#queryActivity" class="query" data-bs-toggle="modal"><i class="bi bi-eye-fill"></i>
-        // </td>
+    });
+    tbody.innerHTML = dataList;
+    console.log(tbody);
 
-
-        tbody.appendChild(row);
-    }
-
-    // 清空分頁控制
+    //建立分頁 -清空分頁
     pagination.innerHTML = "";
-
-    // 計算總頁數
-    const totalPages = Math.ceil(data.length / itemsPerPage);
+    // 總頁數
+    let totalPages = data.totalPage;
+    let currentPage = data.curentPage;
 
     // 創建分頁頁碼
     for (let i = 1; i <= totalPages; i++) {
@@ -122,18 +118,11 @@ function createDataTable(data) {
         }
         const pageLink = document.createElement("a");
         pageLink.classList.add("page-link");
-        pageLink.href = "#";
+        pageLink.href = `${i}`;
         pageLink.textContent = i;
-        pageLink.addEventListener("click", () => {
-            currentPage = i;
-            updateTable();
-        });
         pageItem.appendChild(pageLink);
         pagination.appendChild(pageItem);
     }
+
 }
-
-// 初始化頁面
-updateTable();
-
 /************************ 建立分頁************************/
