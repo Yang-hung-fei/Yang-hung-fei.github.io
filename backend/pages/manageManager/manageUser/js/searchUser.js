71718b479e84fcc2cff75fed376b5123c02ae3b5
Liu-Yun-Syuan <liuyunsyuan1223@gmail.com>
2023-09-04 20:22:58 +0800

    manageUser.js fixing URL problem

--- backend/pages/manageManager/manageUser/js/searchUser.js
+++ backend/pages/manageManager/manageUser/js/searchUser.js
@@ -76,6 +76,7 @@ function toggleSortOrder(orderBy) {
         : "asc"
       : "asc";
   searchURL({ orderBy, sort: newSort });
+  // searchUsers();
 }
 
 // -------------------Fetch-------------------
@@ -125,14 +126,14 @@ function searchUsers() {
       },
     })
       .then((response) => response.json())
-      .then((responseData) => {
+      .then((userData) => {
         // 处理服务器响应数据
-        var code = responseData.code;
-        console.log(responseData);
+        var code = userData.code;
         if (code === 200) {
           // 成功处理响应
-          creatPageButton(responseData.message.size);
-          creatResultTable();
+          console.log(userData);
+          creatPageButton(userData.message);
+          createResultTable(userData.message);
         } else {
           // 处理响应错误
         }
@@ -155,8 +156,9 @@ function searchUsers() {
 
 // -------------------HTML-------------------
 
-function creatPageButton(responsePageSize) {
+function creatPageButton(response) {
   const paginationEl = document.getElementsByClassName("pagination");
+  const responsePageSize = response.size;
   let html = "";
 
   // Create the "Previous" button
@@ -176,21 +178,86 @@ function creatPageButton(responsePageSize) {
     `;
   }
 
-  // Create the "Next" button
-  html += `
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  `;
-
   paginationEl.innerHTML = html;
 }
 
-function creatResultTable() {
-  // 获取分页和内容元素
+function createResultTable(response) {
   const resultTable_el = document.getElementById("resultTable");
+  let html = ``;
+  const userData = response.body;
+
+  // 遍历userData数组
+  userData.forEach((user) => {
+    const userId = user.userId;
+    const userName = user.userName;
+    const userNickName = user.userNickName;
+    const userPoint = user.userPoint;
+    const userBirthday = user.userBirthday || "尚未設定";
+    const userCreated = user.userCreated;
+    const userGender = user.userGender || "尚未設定";
+    const userAddress = user.userAddress || "尚未設定";
+    const userPhone = user.userPhone || "尚未設定";
+    const userPic =
+      user.userPic === null
+        ? "https://i.imgur.com/MqbE2rD.png"
+        : `data:image/png;base64,` + user.userPic;
+    const identityProvider =
+      user.identityProvider === "Google"
+        ? `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="2em"
+      viewBox="0 0 512 512"
+    >
+      <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+      <path
+        d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM164 356c-55.3 0-100-44.7-100-100s44.7-100 100-100c27 0 49.5 9.8 67 26.2l-27.1 26.1c-7.4-7.1-20.3-15.4-39.8-15.4-34.1 0-61.9 28.2-61.9 63.2 0 34.9 27.8 63.2 61.9 63.2 39.6 0 54.4-28.5 56.8-43.1H164v-34.4h94.4c1 5 1.6 10.1 1.6 16.6 0 57.1-38.3 97.6-96 97.6zm220-81.8h-29v29h-29.2v-29h-29V245h29v-29H355v29h29v29.2z"
+      />
+    </svg>
+  `
+        : ``;
+
+    // 创建表格行并添加到html中
+    html += `
+      <tr
+        data-toggle="collapse"
+        data-target="#collapse${userId}"
+        aria-expanded="true"
+        aria-controls="collapse${userId}"
+        class="collapsed"
+      >
+        <th scope="row">${userId}</th>
+        <td>${userName}　(${userNickName})</td>
+        <td>${userPoint}</td>
+        <td>${userBirthday}</td>
+        <td>${userCreated}</td>
+        <td>${identityProvider}</td>
+      </tr>
+      <tr>
+        <td
+          colspan="6"
+          id="collapse${userId}"
+          class="collapse acc"
+          data-parent="#accordion"
+        >
+          <div style="display: flex">
+            <div class="userPic">
+              <img src="${userPic}" alt="userPic" style="background-color: #ececec"/>
+            </div>
+            <div class="userProfile">
+              <div class="userGender">性別：　${userGender}</div>
+              <div class="userPhone">電話：　${userPhone}</div>
+              <div class="userAddress">地址：　${userAddress}</div>
+              <div class="identityProvider"></div>
+            </div>
+          </div>
+        </td>
+      </tr>
+    `;
+  });
+
+  // 将html添加到结果表格中
+  resultTable_el.innerHTML = html;
 }
 
 function deleteTableContent() {
