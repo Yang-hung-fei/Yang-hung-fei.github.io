f3452ea532aa3c283e7ce92bfc60085e597482ca
CHEN YI TING <abc10512106@gmail.com>
2023-09-08 16:11:19 +0800

    add post api

---
+++ frontend/pages/socialMedia/Post/js/messageApi.js
@@ -0,0 +1,102 @@
+import config from "../../../../../ipconfig";
+const hostUrl = config.url;
+const userMesUrl = hostUrl + "/user/social/post";
+
+// create message
+async function createMessage(postId, messageData) {
+    return fetch(userPostUrl + `${postId}/message`, {
+        method: "POST",
+        headers: {
+            Authorization_M: userToken,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(messageData),
+    })
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+}
+
+// edit message
+async function editMessage(messageId, messageData) {
+    return fetch(userPostUrl + `/message/${messageId}`, {
+        method: "PUT",
+        headers: {
+            Authorization_M: userToken,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(messageData),
+    })
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+}
+
+//delete message
+async function deleteMessage(messageId) {
+    return fetch(userPostUrl + `/message/${messageId}`, {
+        method: "DELETE",
+        headers: {
+            Authorization_M: userToken,
+            "Content-Type": "application/json"
+        },
+    })
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+}
+
+//get one message details
+async function getMessage(messageId) {
+    return fetch(userPostUrl + `/message/${messageId}`, {
+        method: "GET",
+        headers: {
+            Authorization_M: userToken,
+            "Content-Type": "application/json"
+        },
+    })
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+}
+
+//get messages by postId 
+async function getMessagesByPostId(postId) {
+    return fetch(userPostUrl + `/${postId}/messages`, {
+        method: "GET",
+        headers: {
+            Authorization_M: userToken,
+            "Content-Type": "application/json"
+        },
+    })
+        .then(res => {
+            return res.json();
+        }).then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+}
+
+export { createMessage, editMessage, deleteMessage, getMessage, getMessagesByPostId }
