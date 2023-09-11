da2a149a3f674d88e611102cd80860328aab4213
Yang-hung-fei <x52131221@gmail.com>
2023-09-11 16:04:18 +0800

    ip modify

--- ipconfig.js
+++ ipconfig.js
@@ -1,6 +1,8 @@
+// const config = {
+//   url: 'http://localhost:8080' 
+// };
 const config = {
-  url: 'http://localhost:8080' 
+  url: 'http://alldogcat.ddns.net:8080' 
 };
-
 
 export default config;
